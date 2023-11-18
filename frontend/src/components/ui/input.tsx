0581732e1e086d417c52963fe3f0f7b834f21f72
Milenacosta08 <milena.gondim.costa08@aluno.ifce.edu.br>
2023-11-18 15:19:05 -0300

    frontend: reestruturação do projeto

---
+++ frontend/src/components/ui/input.tsx
@@ -0,0 +1,37 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import { Label } from "./label"
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+    name: string
+    label?: string
+  }
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, name, label, ...props }, ref) => {
+    return (
+      <div>
+        {!!label && (
+          <Label htmlFor={name}>
+            {label}
+          </Label>
+        )}
+        <input
+          name={name}
+          type={type}
+          className={cn(
+            "flex h-8 w-full rounded-md border-b-[1px] border-border_input bg-background_modal px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
--- frontend/src/infra/api.ts
+++
@@ -1,7 +0,0 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//     baseURL: 'http://localhost:3333',
-//     timeout: 1000,
-// // headers: {'X-Custom-Header': 'foobar'}
-// });
