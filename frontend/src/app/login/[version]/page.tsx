848d15e5399df4ea6aaf9d35e057e4a5f80b62ab
Milenacosta08 <milena.gondim.costa08@aluno.ifce.edu.br>
2023-11-21 00:14:21 -0300

    frontend: diferenciação entre usuario cpae e egresso tela login

--- frontend/src/app/login/[version]/page.tsx
+++ frontend/src/app/login/[version]/page.tsx
@@ -4,23 +4,36 @@ import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Login() {
+type LoginProps = {
+  params: {
+    version: "cpae" | "egress"
+  }
+}
+
+export default function Login({ params: { version } }: LoginProps) {
+  const isCpae = version === "cpae"
 
   return (
     <div className="grid grid-cols-5 gap-3 w-screen h-screen">
       <div className="col-span-2 flex flex-col gap-8 items-center justify-center bg-background_modal">
         <h1 className="mb-10 text-5xl font-bold text-blue_primary">Login</h1>
-        <Input type="text" name="user" label="Usuário" className="w-[23rem]" />
+        {isCpae ? (
+          <Input type="text" name="user" label="Usuário" className="w-[23rem]" />
+        ) : (
+          <Input type="email" name="email" label="Email" className="w-[23rem]" />
+        )}
         <Input type="password" name="password" label="Senha" className="w-[23rem]" />
         <div className="flex items-center gap-[5rem]">
           <Checkbox id="rememberMe" label="Lembre de mim"/>
           <Button variant="link" size="sm" className="font-light text-blue_primary">Esqueci a Senha</Button>
         </div>
         <Button variant="default" className="text-[24px]">Entrar</Button>
-        <div className="flex items-center justify-center gap-2 mt-3">
-          <span className="text-blue_primary font-light">Não tem uma conta?</span>
-          <Link href="#" className="text-blue_light">Registre-se</Link>
-        </div>
+        {!isCpae && (
+          <div className="flex items-center justify-center gap-2 mt-3">
+            <span className="text-blue_primary font-light">Não tem uma conta?</span>
+            <Link href="#" className="text-blue_light">Registre-se</Link>
+          </div>
+        )}
       </div>
       <div className="col-span-3 flex items-center justify-center">
         <Image 
