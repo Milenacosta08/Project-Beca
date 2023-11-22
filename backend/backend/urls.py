99c03b9025adc4c5c2a10163d81e1dd66c4db042
CristinaAraujo <cristinaaraujo3388@gmail.com>
2023-11-22 09:18:28 -0300

    backend: crud pos-graduacao

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from cpae import views
 
-router = routers.DefaultRouter()
-router.register(r'cpae', views.CpaeView, 'cpae')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    #path('api/', include('cpae.urls')),
+    path('api/graduation/', include('graduations.urls')),
+
 ]
